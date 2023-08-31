698d2060dba5c4f78694a73c2869901cdc201d2e
Heisenberg <omaradelawad20013@gmail.com>
2023-08-31 07:24:05 +0300

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        primaryGreen: '#28a745',
+        darkGreen: '#11862c',
+      },
+      fontFamily: {
+        sans: ['Bai Jamjuree', 'sans-serif']
+      }
+    }
+  },
+  plugins: [],
+}
