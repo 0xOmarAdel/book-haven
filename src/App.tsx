397040c2b86319659de945b1a451483caff77c94
Heisenberg <omaradelawad20013@gmail.com>
2023-09-16 12:08:09 +0300

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { Routes, Route } from "react-router-dom";
 
-const App = () => {
+import MainLayout from "./pages/MainLayout";
+import ErrorPage from "./pages/ErrorPage";
+import Home from "./pages/Home";
+import Book from "./pages/Book";
+import Category from "./pages/Category";
+import Author from "./pages/Author";
 
+const App: React.FC = () => {
   return (
-    <div>
-    </div>
-  )
+    <>
+      <Routes>
+        <Route path="*" element={<ErrorPage />} />
+        <Route path="/" element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/category/:id" element={<Category />} />
+          <Route path="/book/:id" element={<Book />} />
+          <Route path="/author/:id" element={<Author />} />
+        </Route>
+      </Routes>
+    </>
+  );
 };
 
-export default App
+export default App;
