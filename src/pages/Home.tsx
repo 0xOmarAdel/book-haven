ca6b47c405a3cc145fe6c14cfd5fd46ca58c8bd8
Heisenberg <omaradelawad20013@gmail.com>
2023-10-16 09:50:02 +0300

    added the navbar to the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import Browse from "../components/Browse";
 import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
 import TopRated from "../components/TopRated";
 
 const Home = () => {
   return (
     <>
+      <Navbar />
       <Hero />
       <TopRated />
       <Browse />
