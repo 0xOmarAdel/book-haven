d11bf20d6d1d590f40f90f312f77cda55ac1a7bf
Heisenberg <omaradelawad20013@gmail.com>
2023-10-16 10:25:47 +0300

    updated the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,15 +26,15 @@ const Navbar = () => {
       ref={navRef}
       className={`${
         navIsFixed ? "fixed z-50 bg-white shadow-lg" : "absolute"
-      } w-full py-4 px-16 flex flex-row items-center justify-between`}
+      } w-full py-4 px-16 flex flex-row items-center justify-between font-semibold`}
     >
-      <p className="text-3xl text-gray-700 font-semibold">Book Haven</p>
+      <p className="text-3xl text-gray-700">Book Haven</p>
       <div className="flex flex-row gap-4 text-xl text-gray-600">
         <a href="#top-rated">Top Rated</a>
         <a href="#popular">Popular</a>
       </div>
       <div className="flex flex-row items-center gap-1 text-gray-500 cursor-pointer">
-        <span className="text-xl font-semibold">Browse</span>
+        <span className="text-xl">Browse</span>
         <HiChevronDown className="text-2xl translate-y-0.5" />
       </div>
     </div>
