484096326bbfa594e0801af098407a9ee9aa6c50
Heisenberg <omaradelawad20013@gmail.com>
2023-10-16 06:27:03 +0300

    added the top rated section to the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import Browse from "../components/Browse";
 import Hero from "../components/Hero";
+import TopRated from "../components/TopRated";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <TopRated />
       <Browse />
     </>
   );
